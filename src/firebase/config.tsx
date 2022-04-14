601575e566909a996c866153a2bbbabec747eec9
Kodake <kodake@hotmail.es>
2022-04-14 02:01:40 -0400

    All initial changes about dojo-chat-app

---
+++ src/firebase/config.tsx
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAn3PQ5qU-7EDW4XqNNXHsiRfRr_ScbDoE",
+    authDomain: "genericdb2.firebaseapp.com",
+    projectId: "genericdb2",
+    storageBucket: "genericdb2.appspot.com",
+    messagingSenderId: "771082677938",
+    appId: "1:771082677938:web:e20e5a5c1a225fff43a3ab"
+};
+
+// Init firebase
+firebase.initializeApp(firebaseConfig);
+
+// Init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// Timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
---
+++ src/hooks/useCollection.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState, useRef } from 'react';
+import { projectFirestore } from '../firebase/config';
+import { Transaction } from '../interfaces/appInterfaces';
+
+type WhereFilterOp = [string, any, string];
+type OrderByOp = [string, any | undefined];
+
+export const useCollection = (collection: string, _query: WhereFilterOp, _orderBy: OrderByOp) => {
+    const [documents, setDocuments] = useState<Transaction[] | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    // if we don't use a ref --> infinite loop in useEffect
+    // _query is an array and is 'different' on every function call
+    const query = useRef(_query).current;
+    const orderBy = useRef(_orderBy).current;
+
+    useEffect(() => {
+        // Get the collection ref
+        // let ref = projectFirestore.collection(collection);
+
+        // Get the collection ref using query
+        let ref = projectFirestore.collection(collection).where(...query);
+
+        if (query) {
+            ref = ref.where(...query);
+        }
+
+        if (orderBy) {
+            ref = ref.orderBy(...orderBy);
+        }
+
+        const unsubscribe = ref.onSnapshot((snapshot: any) => {
+            let results: Transaction[] = [];
+
+            snapshot.docs.forEach((doc: any) => {
+                results.push({ ...doc.data(), id: doc.id })
+            });
+
+            // Update state
+            setDocuments(results);
+            setError(null);
+        }, (error: any) => {
+            console.log(error);
+            setError('Could not fetch the data');
+        });
+
+        // Unsubscribe from events when the component unmounts
+        return () => {
+            unsubscribe();
+        }
+
+    }, [collection, query, orderBy]);
+
+    return { documents, error };
+
+}
